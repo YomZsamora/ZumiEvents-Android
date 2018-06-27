11f59b38ffe7c1cbc2a066bee67bbd11744904a5
yomZsamora <samora.y@adzumi.co.ke>
2018-06-27 16:12:51 +0300

    Finished MainActivity login page.

--- app/src/main/java/com/adzumi/zumievents/ui/UserRegistrationActivity.java
+++ app/src/main/java/com/adzumi/zumievents/ui/UserRegistrationActivity.java
@@ -1,13 +1,15 @@
-package com.adzumi.zumievents;
+package com.adzumi.zumievents.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.adzumi.zumievents.R;
+
+public class UserRegistrationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_user_registration);
     }
 }
