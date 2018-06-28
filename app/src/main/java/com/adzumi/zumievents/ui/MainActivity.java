e62374442b74baa2e3bf8c81060ded2b5ccf8682
yomZsamora <samora.y@adzumi.co.ke>
2018-06-28 07:53:04 +0300

    UserRegistrationActivity done.

--- app/src/main/java/com/adzumi/zumievents/ui/MainActivity.java
+++ app/src/main/java/com/adzumi/zumievents/ui/MainActivity.java
@@ -1,15 +1,37 @@
 package com.adzumi.zumievents.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.adzumi.zumievents.R;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mRegisterTextView;
+//    @BindView(R.id.registerTextView) TextView mRegisterTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        ButterKnife.bind(this);
+        mRegisterTextView = (TextView) findViewById(R.id.registerTextView);
+
+        mRegisterTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        if (view == mRegisterTextView) {
+            Intent intent = new Intent(MainActivity.this, UserRegistrationActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
--- app/src/main/java/com/adzumi/zumievents/ui/UserRegistrationActivity.java
+++ app/src/main/java/com/adzumi/zumievents/ui/UserRegistrationActivity.java
@@ -1,15 +1,31 @@
 package com.adzumi.zumievents.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.adzumi.zumievents.R;
 
-public class UserRegistrationActivity extends AppCompatActivity {
+public class UserRegistrationActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mLoginuser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_registration);
+
+        mLoginuser = (TextView) findViewById(R.id.loginTextView);
+        mLoginuser.setOnClickListener(this);
+    }
+
+    public void onClick(View view){
+        if(view == mLoginuser){
+            Intent intent = new Intent(UserRegistrationActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
