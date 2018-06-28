04020fb4f0c5b3834a8eb603b34cfe1d9b4eb7b9
yomZsamora <samora.y@adzumi.co.ke>
2018-06-28 16:00:36 +0300

    Added back button to HomeActivity.

--- app/src/main/java/com/adzumi/zumievents/ui/MainActivity.java
+++ app/src/main/java/com/adzumi/zumievents/ui/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.adzumi.zumievents.R;
@@ -14,6 +15,7 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @BindView(R.id.registerTextView) TextView mRegisterTextView;
+    @BindView(R.id.passwordLoginButton) Button mPasswordLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +24,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ButterKnife.bind(this);
 
         mRegisterTextView.setOnClickListener(this);
+        mPasswordLoginButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view){
         if (view == mRegisterTextView) {
             Intent intent = new Intent(MainActivity.this, UserRegistrationActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        if (view == mPasswordLoginButton) {
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
             startActivity(intent);
             finish();
         }
