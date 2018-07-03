4650bba09cc2abbe1d693708a83d95b12a9cd919
yomZsamora <samora.y@adzumi.co.ke>
2018-07-03 16:39:45 +0300

    User Authentication Success.

--- app/src/main/java/com/adzumi/zumievents/ui/UserRegistrationActivity.java
+++ app/src/main/java/com/adzumi/zumievents/ui/UserRegistrationActivity.java
@@ -1,13 +1,22 @@
 package com.adzumi.zumievents.ui;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.adzumi.zumievents.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,13 +28,20 @@ public class UserRegistrationActivity extends AppCompatActivity implements View.
     @BindView(R.id.emailEditText) EditText mEmailEditText;;
     @BindView(R.id.passwordEditText) EditText mPasswordEditText;
     @BindView(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
-    @BindView(R.id.createUserButton) EditText mCreateUserButton;
+    @BindView(R.id.createUserButton) Button mCreateUserButton;
+
+    public static final String TAG = UserRegistrationActivity.class.getSimpleName();
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_registration);
         ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        createAuthStateListener();
 
         mLoginuser.setOnClickListener(this);
         mCreateUserButton.setOnClickListener(this);
@@ -40,5 +56,42 @@ public class UserRegistrationActivity extends AppCompatActivity implements View.
         if (view == mCreateUserButton) {
             createNewUser();
         }
+    }
+
+    private void createNewUser() {
+        final String name = mNameEditText.getText().toString().trim();
+        final String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        String confirmPassword = mConfirmPasswordEditText.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Authentication successful");
+                        } else {
+                            Toast.makeText(UserRegistrationActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void createAuthStateListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(UserRegistrationActivity.this, HomeActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+
+        };
     }
 }
